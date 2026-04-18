6d2cb776df7094d7a5dc71bb377bb9bc6079ed2d
vebg25 <123148096+vebg25@users.noreply.github.com>
2026-04-18 17:13:02 +0530

    Update solution for logistic-regression-training

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -9,27 +9,21 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     Train logistic regression via gradient descent.
     Return (w, b).
     """
-    # Write code here
     X = np.array(X)
     y = np.array(y)
 
     m, n = X.shape
-    # m --> Samples
-    # n --> features
 
     w = np.zeros(n)
     b = 0
-
-    
-
     for i in range(steps):
-        z = X @ w + b
+        z = X@w + b
         p = _sigmoid(z)
-        
-        weight_game = (X.T @ (p-y))/m
-        bias_game = np.sum(p-y)/m
 
-        w = w - lr * weight_game
-        b = b - lr * bias_game
+        dw = ((p-y)@X)/m
+        db = np.sum(p-y)/m
+
+        w = w - lr * dw 
+        b = b - lr * db
 
     return w, b
