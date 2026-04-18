e63f73bb5278a1024f4fc7bfd3e702fe6b1bfc39
vebg25 <123148096+vebg25@users.noreply.github.com>
2026-04-18 17:37:21 +0530

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -7,10 +7,16 @@ def cross_entropy_loss(y_true, y_pred):
     # Write code here
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
+    
     sum = 0
-    for i in range(len(y_true)):
-        p = y_pred[i][y_true[i]]
-        log_p =-np.log(p)
-        sum+=log_p
+    
+    m = len(y_true)
+    
+    true_class_probs = y_pred[np.arange(m), y_true]
+    return np.mean(-np.log(true_class_probs))
+    # for i in range(len(y_true)):
+    #     p = y_pred[i][y_true[i]]
+    #     log_p =-np.log(p)
+    #     sum+=log_p
         
-    return sum/len(y_true)
+    # return sum/len(y_true)
