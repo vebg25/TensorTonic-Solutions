7373f82d2c279e0fb5014b78d9ee9b53516e33a2
vebg25 <123148096+vebg25@users.noreply.github.com>
2026-04-18 17:38:53 +0530

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -9,7 +9,7 @@ def cross_entropy_loss(y_true, y_pred):
     y_pred = np.array(y_pred)
     
     m = len(y_true)
-    
+    # This creates a list of [0,1,2] if m is 3 and then y_true is concatenated as the column element and together they become dynamic indexing for y_pred
     true_class_probs = y_pred[np.arange(m), y_true]
     return np.mean(-np.log(true_class_probs))
     # sum = 0
